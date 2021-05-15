f90561d1ddbbaa47dfff029c01c6fab0f8f86fca
王腾龙 <wb-wtl01056843@alibaba-inc.com>
2021-05-15 22:02:29 +0800

    demo

---
+++ learning-demo/src/main/java/thread/CountDownLatchTest.java
@@ -0,0 +1,54 @@
+package thread;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
+
+public class CountDownLatchTest {
+
+    @Test
+    public void test1() {
+
+//        if (1==1 && 1==1 || 1!=2){
+//            int a =1;
+//        }
+//        int abc=1;
+////        CountDownLatch countDownLatch = new CountDownLatch(1);
+////
+////        countDownLatch.countDown();
+////        countDownLatch.countDown();
+////
+////        int a = 1;
+//
+//        List add = new ArrayList();
+//        add.add("92900002_3_2_12_");
+//        add.add("92900002_3_3_12_");
+//
+//
+//        List exit = new ArrayList();
+//        exit.add("92900002_3_3_12_");
+//
+//        List aa = (List) add.stream().filter(e->exit.contains(e)).collect(Collectors.toList());
+//
+//        if (add.retainAll(exit)) {
+////            int aa = 1;
+//        } else {
+//            int a = 2;
+//        }
+
+        if (1 == 1) {
+            int a = 1;
+        }
+        else if (1 == 1) {
+            int a = 1;
+        }
+       else if (1 == 1) {
+            int a = 1;
+        } else {
+            int a = 1;
+        }
+    }
+}
